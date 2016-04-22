6e8b2a934117e325a4206cfc65b12341c49ae3fb
Chris Stoy <cstoy@circlegraphicsonline.com>
2016-04-22 14:58:50 -0400

    adding base svgtest project

---
+++ svgtest/app/app.js
@@ -0,0 +1,11 @@
+(function () {
+	'use strict';
+
+// Declare app level module which depends on views, and components
+	angular.module('myApp', [
+		'ngRoute',
+	]).config(['$routeProvider', function ($routeProvider) {
+		$routeProvider.otherwise({redirectTo: '/'});
+	}]);
+
+})();
